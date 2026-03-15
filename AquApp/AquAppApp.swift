de8ac5628908ac2bd264e0386a63025b88e992e1
HarpePluie <HarpePluie@icloud.com>
2026-03-15 12:10:08 +0100

    Initial Commit

---
+++ AquApp/AquAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  AquAppApp.swift
+//  AquApp
+//
+//  Created by Fabian Dargaud on 15/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AquAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AquApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AquApp
+//
+//  Created by Fabian Dargaud on 15/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
