216e1e4b18b7d78f5123a4b335d5c673aa6e4670
HarpePluie <HarpePluie@icloud.com>
2026-09-02 18:54:08 +0200

    Fix défi Grand Buveur : utilise todayWaterMlRaw au lieu de todayWaterMl

--- AquApp/Views/Challenges/ChallengesView.swift
+++ AquApp/Views/Challenges/ChallengesView.swift
@@ -23,12 +23,6 @@ enum ChallengeCategory: String {
     }
 }
 
-enum ChallengeStatus {
-    case locked
-    case available
-    case inProgress
-    case completed
-}
 
 // MARK: - Challenge Model
 
